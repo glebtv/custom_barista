31344fbc7cde088d4067a74a69c56baccfa259be
Gleb Tv <glebtv@gmail.com>
2018-04-05 13:23:55 +0300

    add week day

--- ltime/ltime.go
+++ ltime/ltime.go
@@ -15,7 +15,7 @@ func Get() bar.Module {
 	return clock.New().OutputFunc(func(now time.Time) bar.Output {
 		return outputs.Pango(
 			material.Icon("today", colors.Scheme("dim-icon")),
-			now.Format("2006-01-02 "),
+			now.Format("Mon 2006-01-02 "),
 			material.Icon("access-time", colors.Scheme("dim-icon")),
 			now.Format("15:04:05"),
 		)
