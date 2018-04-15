ef76f24bbbdedbc589e949a29af3bb1597ebb659
glebtv <glebtv@gmail.com>
2018-04-15 13:10:28 +0300

    fix temperature

--- weather/weather.go
+++ weather/weather.go
@@ -60,7 +60,7 @@ func Get() weather.Module {
 		}
 		return outputs.Pango(
 			typicons.Icon(iconName), utils.Spacer,
-			pango.Textf("%d℃", w.Temperature.Celsius()),
+			pango.Textf("%.1f℃", w.Temperature.Celsius()),
 			//pango.Span(" (provided by ", w.Attribution, ")", pango.XSmall),
 		)
 	})
